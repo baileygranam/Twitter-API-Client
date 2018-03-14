d4c410fe6319628dcd517b0f8ec30eb386e5424f
Bailey Granam <bgranam@stetson.edu>
2018-03-14 14:22:30 -0400

    Removed vendor folder.

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+	
+	/* Define Twitter Access Keys/Tokens. */
+    define('OAUTH_ACCESS_TOKEN', '744391147884273664-k4HVkfq4oCQtvm0zRzoBv4SOX9OkKzx');
+    define('OAUTH_ACCESS_TOKEN_SECRET', 'WWJ47e2gZaUOvjOHzqgqJbyGKfAJ2pletRLkdCYBYiYjz');
+    define('CONSUMER_KEY', 'VEKAllSJ4Kohy28sMUsRZRXXs');
+    define('CONSUMER_SECRET', 'BZpt2UwcOWFwkodFFuKXgBjqmDQBEuNMVv4e3JXBNdzlvy7POj');
