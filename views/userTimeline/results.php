640f4d6930dad376d03183b06c401bef33091947
Bailey Granam <bgranam@stetson.edu>
2018-03-19 09:38:47 -0400

    Tests to access API

--- views/userTimeline/results.php
+++ views/userTimeline/results.php
@@ -1,12 +1,14 @@
 
 <?php
 
+
 foreach($this->userTimeline as $tweet)
-
 {
+	//print_r($tweet);
 
-  echo "	{$tweet->text} {$tweet->created_at}\n";
+  echo "{$tweet->text} {$tweet->created_at}<br>";
 
+  print ($tweet->entities->media[0]->media_url);
 }
 
 ?>
