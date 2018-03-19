69a648f3cedfdcd2f5194b586bf083ca2dd0bc6b
Bailey Granam <bgranam@stetson.edu>
2018-03-19 13:00:39 -0400

    Added user profile image to timeline tweet results.

--- views/userTimeline/results.php
+++ views/userTimeline/results.php
@@ -5,18 +5,16 @@
 			<?php foreach($this->userTimeline as $tweet) { ?>
 			<div class="card" style="width: 35rem;">
 				<div class="card-header">
+					<span id="profile-image"><img src="<?php echo $tweet->user->profile_image_url; ?>"></span>
 			    	<span id="name"><?php echo $tweet->user->name; ?></span>
-			    	<span id="screen-name"> &#8211; @<?php echo $tweet->user->screen_name; ?> </span>
-			    	<span><?php 
-$date = new DateTime($tweet->created_at);
-
-			    	echo $date->format('M j'); ?></span>
+			    	<a id="screen-name" href="<?php echo $tweet->user->url; ?>"> &#8211; @<?php echo $tweet->user->screen_name; ?> </a>
+			    	<span id="date"><?php echo (new DateTime($tweet->created_at))->format('M j'); ?></span>
 			  	</div>
+			  	<?php if(isset($tweet->entities->media)){ ?>
 				<img class="card-img-top" src="<?php echo $tweet->entities->media[0]->media_url; ?>" alt="Card image cap">
+				<?php }?>
 				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
 					<p class="card-text"><?php echo $tweet->text; ?></p>
-					<a href="#" class="btn btn-primary">Go somewhere</a>
 				</div>
 			</div>
 			<?php } ?>
