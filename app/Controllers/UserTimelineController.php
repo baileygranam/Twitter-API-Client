e3f6449b3f85b2683f428e5d19a92255ced96ba3
Bailey Granam <bgranam@stetson.edu>
2018-03-20 09:04:35 -0400

    Removed timeline parsing method and placed it in the userTimeline Model.

--- app/Controllers/UserTimelineController.php
+++ app/Controllers/UserTimelineController.php
@@ -46,59 +46,7 @@ class UserTimelineController
 
         /* Load/render and display the results to the user. */
         $view = new Renderer('views/userTimeline/');
-        $view->userTimeline = $this->parseTimeline($userTimeline);
+        $view->userTimeline = $userTimeline;
         $view->render('results.php');
-    }
-
-    /**
-    * Method to parse the timeline received by the Twitter API.
-    * @param $userTimeLine - timeline received by the Twitter API.
-    * @return parsed timeline.
-    */
-    private function parseTimeline($userTimeline)
-    {
-        /* Loop through each tweet. */
-        foreach($userTimeline as $tweet) 
-        {
-            /* If the tweet is a retweet then format it specific for retweets. */
-            if (isset($tweet->retweeted_status)) 
-            {
-                $tweet_text = "RT <a href='https://twitter.com/{$tweet->retweeted_status->user->screen_name}'>@{$tweet->retweeted_status->user->screen_name}</a>: 
-                {$tweet->retweeted_status->full_text}";
-                $favorites = $tweet->retweeted_status->favorite_count;
-            } 
-            else 
-            {
-                $tweet_text = $tweet->full_text;
-                $favorites = $tweet->favorite_count;
-            }
-
-            /* Check to see if the tweet has a media url. */
-            if(isset($tweet->entities->media))
-            {
-                $media_url = $tweet->entities->media[0]->media_url;
-            }
-            else
-            {
-                $media_url = "";
-            }
-    
-            /* Parse the data. */
-            $twitter_feed[] = array(
-                'text' => $tweet_text, 
-                'name' => $tweet->user->name,
-                'username' => $tweet->user->screen_name,
-                'user-profile-img' => $tweet->user->profile_image_url,
-                'media-url' => $media_url,
-                'date-created' => (new \DateTime(($tweet->created_at)))->format('M j'),
-                'favorites' => $favorites,
-                'retweets' => $tweet->retweet_count,
-                'link' => "http://twitter.com/".$tweet->user->screen_name."/status/".$tweet->id
-            );
- 
-        unset($tweet_text);
-        }
-
-        return $twitter_feed;
     }
 }
