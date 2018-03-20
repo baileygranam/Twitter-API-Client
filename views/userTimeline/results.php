947ac7a27b42149b3718dd8215a98ae74ffce27b
Bailey Granam <bgranam@stetson.edu>
2018-03-19 22:23:56 -0400

    Changed userTimeline view to reflect new parsing format declared in the controller.

--- views/userTimeline/results.php
+++ views/userTimeline/results.php
@@ -6,23 +6,25 @@
 			<div class="card" style="width: 40rem;">
 				<div class="card-header">
 					<a href="<?php echo $tweet->user->url; ?>">
-						<span id="profile-image"><img src="<?php echo $tweet->user->profile_image_url; ?>"></span>
-			    		<span id="name"><?php echo $tweet->user->name; ?></span>
-			    		<span id="screen-name"> &#8211; @<?php echo $tweet->user->screen_name; ?> </span>
+						<span id="profile-image"><img src="<?php echo $tweet['user-profile-img']; ?>"></span>
+			    		<span id="name"><?php echo $tweet['name']; ?></span>
+			    		<span id="screen-name"> &#8211; @<?php echo $tweet['username']; ?> </span>
 			    	</a>
-			    	<span id="date"><?php echo (new DateTime($tweet->created_at))->format('M j'); ?></span>
+			    	<span id="date"><?php echo (new DateTime($tweet['date-created']))->format('M j'); ?></span>
 			  	</div>
-			  	<?php if(isset($tweet->entities->media)){ ?>
-				<img class="card-img-top" src="<?php echo $tweet->entities->media[0]->media_url; ?>" alt="Card image cap">
+			  	<?php if(!empty($tweet['media-url'])){ ?>
+				<img class="card-img-top" src="<?php echo $tweet['media-url']; ?>" alt="Card image cap">
 				<?php }?>
 				<div class="card-body">
-					<p class="card-text"><?php echo $tweet->full_text; ?></p>
+					<p class="card-text"><?php echo $tweet['text']; ?></p>
 					<span id="tweet-stats">
 						<img src="https://image.flaticon.com/icons/svg/126/126471.svg" width="18">
-						<?php echo $tweet->favorite_count; ?>
+						<?php echo $tweet['favorites']; ?>
 						<img src="https://image.flaticon.com/icons/svg/127/127998.svg" width="18">
-						<?php echo $tweet->retweet_count; ?>
-						<img src="https://image.flaticon.com/icons/svg/126/126481.svg" width="18">
+						<?php echo $tweet['retweets']; ?>
+						<a href="<?php echo $tweet['link']; ?>" target="_blank">
+							<img src="https://image.flaticon.com/icons/svg/126/126481.svg" width="18">
+						</a>
 					</span>
 				</div>
 			</div>
