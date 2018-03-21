a91710c546941567229590a8cdd39f065569e43c
Bailey Granam <bgranam@stetson.edu>
2018-03-21 12:40:34 -0400

    Updated main home file.

--- views/main/home.php
+++ views/main/home.php
@@ -1,2 +1,20 @@
-<h1>The Competition Begins!</h1>
-<p>Welcome to the <?=$this->title?>!<p>
+<div class="container">
+    <div class="row justify-content-md-center">
+    	<div class="col-md-auto">
+			<div class="card" style="max-width:40em; width:auto">
+  				<div class="card-header">
+    				<h5 class="card-title">Welcome to the Twitter API integration!</h5>
+    				<h6 class="card-subtitle mb-2 text-muted">Created By: Bailey Granam</h6>
+    			</div>
+    			<div class="card-body">
+    				<p class="card-text">This web integration of the Twitter API contains features such as searching a user's timeline, searching for users, and searching by hashtag/phrase.</p>
+    			</div>
+    			<div class="card-footer">
+    				<a class="card-link" href="?controller=userTimeline&action=index"><i class="fas fa-list-ul"></i> Timelines</a>
+		      		<a class="card-link" href="?controller=searchTweets&action=index"><i class="fas fa-search"></i> Tweet Search</a>
+		      		<a class="card-link" href="?controller=searchUsers&action=index"><i class="fas fa-user"></i> User Search</a>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>
